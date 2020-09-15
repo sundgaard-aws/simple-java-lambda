8c151976bf59dd3904b5203a4db30420080cd483
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2020-09-15 14:19:58 +0200

    More files

---
+++ src/main/com/aws/sundgaar/SimpleHandler.java
@@ -0,0 +1,18 @@
+package com.aws.sundgaar;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.aws.codestar.projecttemplates.GatewayResponse;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleHandler {
+    public Object handleRequest(final Object input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return new GatewayResponse(new JSONObject().put("Output", "Hello World, this Lambda function is deployed via CodeStar!").toString(), headers, 200);
+    }
+}
